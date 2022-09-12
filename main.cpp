38b55d5415cbfd18956eec96e70a33d657e00ce3
DaffaRozan <112892515+DaffaRozan@users.noreply.github.com>
2022-09-12 19:35:52 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Daffa Rozan F" << endl;
+    /*
+    gjsdgfjs
+    dhfshdfj
+    dgfgjdfk
+    */
+
+    int u = 18;
+    //bilangan bulat
+    float t = 178.1;
+    //bilangan pecahan
+    char b = 'L';
+    // 1 huruf
+    string ub = "XL";
+    // lebih dari 1 huruf/kata
+    cout << "umur saya: " << u;
+    cout << endl;
+    cout << "tinggi saya: " << t;
+    cout << "\nUkuran baju: " << b;
+    cout << "\nUkuran baju: " << ub;
+
+
+    return 0;
+}
